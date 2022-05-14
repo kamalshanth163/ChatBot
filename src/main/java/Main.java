0f41d8bfd296a8fe758481f7992195a74df93d77
Kamalshanth Selvarajah <kamalshanthdeveloper163@gmail.com>
2022-05-14 18:26:07 +0530

    add chat window

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,9 +8,13 @@ public class Main {
         System.out.println("Started the application");
 
         Spark.get("/", (request, response) -> {
-
             HashMap<String, Object> model = new HashMap<>();
             return new ThymeleafTemplateEngine().render(new ModelAndView(model, "index"));
+        });
+
+        Spark.get("/chat", (request, response) -> {
+            HashMap<String, Object> model = new HashMap<>();
+            return new ThymeleafTemplateEngine().render(new ModelAndView(model, "chat-window"));
         });
 
     }
