1e86db4a1027a7f8e4b24de8f1b2e6b95fc37f16
Kamalshanth Selvarajah <kamalshanthdeveloper163@gmail.com>
2022-05-14 17:43:35 +0530

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Started the application");
+
+        Spark.get("/", (request, response) -> {
+
+            HashMap<String, Object> model = new HashMap<>();
+            return new ThymeleafTemplateEngine().render(new ModelAndView(model, "index"));
+        });
+
+    }
+}
